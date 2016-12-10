cd1d90ee88bc1f37c2e00906a93c9220cc8d35ed
Koen Hendriks <info@koenhendriks.com>
2016-12-10 23:17:09 +0100

    Fixed date selecting and displaying

--- src/pages/date/date.ts
+++ src/pages/date/date.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { ResultsPage } from '../results/results'
+import { ResultsPage } from '../results/results';
 import { DatePicker } from 'ionic-native';
 
 
@@ -15,21 +15,71 @@ import { DatePicker } from 'ionic-native';
   templateUrl: 'date.html'
 })
 export class DatePage {
+	dateToInput:any;
+	dateFromInput:any;	
 
   constructor(public navCtrl: NavController) {
-  	
+  	this.dateFromInput = 'Zo, 11 Dec,2016';
+  	this.dateToInput = 'Za, 17 Dec, 2016';
   }
 
-  nextPage(){
+  dateTo(event: Event){
+  	event.preventDefault();
+
   	DatePicker.show({
-	  date: new Date(),
+	  date: new Date(),  
 	  mode: 'date',
 	  androidTheme: 5
-	}).then(
-	  date => console.log('Got date: ', date),
+	}).then(date => {
+		// di 13 dec. 2016
+		console.log(typeof date);
+	  	console.log(date.toLocaleDateString('nl-NL', { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' })); // Wed Dec 14 2016	  	
+	  	var splitter = date.toLocaleDateString('nl-NL', { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' }); // Wed Dec 14 2016	  	
+
+	  	var boom = splitter.split(' ');
+	  	console.log(boom);
+	  	var day = this.ucfirst(boom[0]);
+	  	var month = this.ucfirst(boom[2].slice(0,-1));
+	  	var dat = this.ucfirst(boom[1]);
+	  	var year = this.ucfirst(boom[3]);
+  		this.dateToInput = day+', '+dat+' '+month+', '+year;
+	  },
 	  err => console.log('Error occurred while getting date: ', err)
 	);
-  	//this.navCtrl.push(ResultsPage, {}, {animate: true, direction:'forward', animation:'ios-transition'});
+  }
+
+  dateFrom(event: Event){
+  	event.preventDefault();
+
+  	DatePicker.show({
+	  date: new Date(),  
+	  mode: 'date',
+	  androidTheme: 5
+	}).then(date => {
+		// di 13 dec. 2016
+		console.log(typeof date);
+	  	console.log(date.toLocaleDateString('nl-NL', { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' })); // Wed Dec 14 2016	  	
+	  	var splitter = date.toLocaleDateString('nl-NL', { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' }); // Wed Dec 14 2016	  	
+
+	  	var boom = splitter.split(' ');
+	  	console.log(boom);
+	  	var day = this.ucfirst(boom[0]);
+	  	var month = this.ucfirst(boom[2].slice(0,-1));
+	  	var dat = this.ucfirst(boom[1]);
+	  	var year = this.ucfirst(boom[3]);
+  		this.dateFromInput = day+', '+dat+' '+month+', '+year;
+	  },
+	  err => console.log('Error occurred while getting date: ', err)
+	);
+  }
+
+  ucfirst(str) {
+    var firstLetter = str.substr(0, 1);
+    return firstLetter.toUpperCase() + str.substr(1);
+}
+
+  nextPage(){
+  	this.navCtrl.push(ResultsPage, {}, {animate: true, direction:'forward', animation:'ios-transition'});
   }
 
 }
