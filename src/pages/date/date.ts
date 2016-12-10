60c5efd110bc168bd77d54d95415ae0cba4779e3
Koen Hendriks <info@koenhendriks.com>
2016-12-10 21:12:24 +0100

    Fixed style for date

--- src/pages/date/date.ts
+++ src/pages/date/date.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ResultsPage } from '../results/results'
+import { DatePicker } from 'ionic-native';
+
+
 /*
   Generated class for the Date page.
 
@@ -13,10 +16,20 @@ import { ResultsPage } from '../results/results'
 })
 export class DatePage {
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public navCtrl: NavController) {
+  	
+  }
 
   nextPage(){
-  	this.navCtrl.push(ResultsPage, {}, {animate: true, direction:'forward', animation:'ios-transition'});
+  	DatePicker.show({
+	  date: new Date(),
+	  mode: 'date',
+	  androidTheme: 5
+	}).then(
+	  date => console.log('Got date: ', date),
+	  err => console.log('Error occurred while getting date: ', err)
+	);
+  	//this.navCtrl.push(ResultsPage, {}, {animate: true, direction:'forward', animation:'ios-transition'});
   }
 
 }
