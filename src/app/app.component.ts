c9fb2002ea58aae0dabe9ff1830f9dff520e3979
Koen Hendriks <info@koenhendriks.com>
2016-12-11 01:35:47 +0100

    Finished static card style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
-import { HomePage } from '../pages/home/home';
+import { ResultsPage } from '../pages/results/results';
 
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = HomePage;
+  rootPage = ResultsPage;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
