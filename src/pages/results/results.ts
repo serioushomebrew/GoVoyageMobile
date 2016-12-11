0e3d831816d889ace157bf16c2d009141a1a0367
Koen Hendriks <info@koenhendriks.com>
2016-12-11 02:52:05 +0100

    Testing API calls

--- src/pages/results/results.ts
+++ src/pages/results/results.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
 
 /*
   Generated class for the Results page.
@@ -12,11 +13,36 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'results.html'
 })
 export class ResultsPage {
+	data: any;
+  constructor(public navCtrl: NavController, public http: Http) {
 
-  constructor(public navCtrl: NavController) {}
+  		this.data = {};
+        this.data.username = '';
+        this.data.response = '';
+
+		var base ='https://govoyage.nl/api/searchflights';
+		
+		var postdata = {
+			start : '10-12-2016',
+			end : '28-12-2016',
+			budget : '600',
+			passengers : '2',
+			temperature : '15',
+		}
+		
+		this.http.post(base, postdata)
+	        .subscribe(data => {
+	            console.log(data);
+	            console.log(typeof data);
+	            
+	        }, error => {
+	            console.log(JSON.stringify(error.json()));
+	        });
+	  }
 
   ionViewDidLoad() {
-    console.log('Hello ResultsPage Page');
-  }
+	
+  }	    
+
 
 }
